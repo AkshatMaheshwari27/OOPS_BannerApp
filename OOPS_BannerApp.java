346182ef5b05200800cae1cde1f0409e46c75cbd
Akshat Maheshwari <akshatmaheshwari277@gmail.com>
2026-03-03 15:09:28 +0530

    Complete uc6

--- OOPS_BannerApp.java
+++ OOPS_BannerApp.java
@@ -1,8 +1,52 @@
 public class OOPS_BannerApp 
 {
-       public static void main(String[] args) 
-       {
-        System.out.println("OOPS");
-       }
-}
+    public static String[] getO() {
+        return new String[] {
+            "  ****** ",
+            " **    **",
+            " **    **",
+            " **    **",
+            " **    **",
+            " **    **",
+            "  ****** "
+        };
+    }
 
+    // Letter P (9 characters wide)
+    public static String[] getP() {
+        return new String[] {
+            " ******  ",
+            " **    **",
+            " **    **",
+            " ******  ",
+            " **       ",
+            " **       ",
+            " **       "
+        };
+    }
+
+    // Letter S (9 characters wide)
+    public static String[] getS() {
+        return new String[] {
+            "  ****** ",
+            " **      ",
+            " **      ",
+            "  *****  ",
+            "      ** ",
+            "      ** ",
+            " ******  "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o1 = getO();
+        String[] o2 = getO();
+        String[] p  = getP();
+        String[] s  = getS();
+
+        for (int i = 0; i < o1.length; i++) {
+            System.out.println(o1[i] + "  " + o2[i] + "  " + p[i] + "  " + s[i]);
+        }
+    }
+}
