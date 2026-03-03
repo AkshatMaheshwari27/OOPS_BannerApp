bc234a1ac5a3ef45760a8f4372bbb8d49faeb5d1
Akshat Maheshwari <akshatmaheshwari277@gmail.com>
2026-03-03 14:35:15 +0530

    Fix: updating with the correct code

--- OOPS_BannerApp.java
+++ OOPS_BannerApp.java
@@ -2,14 +2,65 @@ public class OOPS_BannerApp
 {
     public static void main(String[] args) 
     {
+        String row1 = String.join("   ",
+                " *** ",
+                " *** ",
+                " *** ",
+                " *** "
+        );
+
+        String row2 = String.join("   ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*    "
+        );
+
+        String row3 = String.join("   ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*    "
+        );
+
+        String row4 = String.join("   ",
+                "*   *",
+                "*   *",
+                " *** ",
+                " *** "
+        );
+
+        String row5 = String.join("   ",
+                "*   *",
+                "*   *",
+                "*    ",
+                "    *"
+        );
+
+        String row6 = String.join("   ",
+                "*   *",
+                "*   *",
+                "*    ",
+                "    *"
+        );
+
+        String row7 = String.join("   ",
+                " *** ",
+                " *** ",
+                "*    ",
+                " *** "
+        );
+
+        // Print banner
         System.out.println(String.join("\n",
-                " ***    ***    ***    *** ",
-                "*   *  *   *  *   *  *    ",
-                "*   *  *   *  *   *  *    ",
-                "*   *  *   *  ***    ***  ",
-                "*   *  *   *  *         * ",
-                "*   *  *   *  *         * ",
-                " ***    ***   *      *** "
+                row1,
+                row2,
+                row3,
+                row4,
+                row5,
+                row6,
+                row7
         ));
     }
+}
 }
